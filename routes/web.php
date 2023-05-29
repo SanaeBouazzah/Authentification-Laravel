3b3460581ea248cff26b19ce5f96bfae7ec79a42
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-29 17:00:32 +0200

    Website-Laravel

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [StaticController::class, 'index'])->name('home');
+Route::get('/about', [StaticController::class, 'about'])->name('about');
+
+
 Route::get('/about', function () {
     return view('about');
 });
