0a4ac736f910dc5d253f040de5b9a169e07f2864
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-29 16:48:41 +0200

    Website-Laravel

--- resources/views/customers.blade.php
+++ resources/views/customers.blade.php
@@ -2,6 +2,6 @@
 @section('content')
 <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center 
 bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-  <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Documentation</h2>
+  <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Customers</h2>
 </div>
 @endsection
