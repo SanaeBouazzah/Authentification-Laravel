77d1265bb6c3d22ef17e673f5ce45e02b31a6b28
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-29 17:03:14 +0200

    Website-Laravel

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [StaticController::class, 'index'])->name('home');
 Route::get('/about', [StaticController::class, 'about'])->name('about');
 Route::get('/customers', [StaticController::class, 'customers'])->name('customers');
-Route::get('/about', [StaticController::class, 'about'])->name('about');
+Route::get('/contact', [StaticController::class, 'contact'])->name('contact');
 
 
 // Route::get('/about', function () {
