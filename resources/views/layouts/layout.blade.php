9482578fbcffde0ee3eb0cac4afba7142cc26056
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-29 17:03:47 +0200

    Website-Laravel

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -12,8 +12,8 @@
       <nav class="flex sm:justify-center space-x-4">
         <a href="{{route('home')}}" class="text-xl font-semibold text-gray-900 dark:text-white">Home</a>
         <a href="{{route('about')}}" class="text-xl font-semibold text-gray-900 dark:text-white">About</a>
-        <a href="/customers" class="text-xl font-semibold text-gray-900 dark:text-white">Customers</a>
-        <a href="/contact" class="text-xl font-semibold text-gray-900 dark:text-white">Contact</a>
+        <a href="{{route('customers')}}" class="text-xl font-semibold text-gray-900 dark:text-white">Customers</a>
+        <a href="{{route('contact')}}" class="text-xl font-semibold text-gray-900 dark:text-white">Contact</a>
       </nav>
 
       @yield('content')
