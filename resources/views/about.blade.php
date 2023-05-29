1dc3d71e991efa0382473c77b826a0db628462ed
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-29 16:54:18 +0200

    Website-Laravel

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,4 +1,7 @@
 @extends('layouts.layout')
+@section('title')
+  About
+@endsection
 @section('content')
 <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center 
 bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
