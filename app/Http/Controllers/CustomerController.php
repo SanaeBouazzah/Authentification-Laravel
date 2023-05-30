52f9ed6678456184ec7e73ed43ca55a9ee66fbd2
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-31 00:17:07 +0200

    Website-laravel

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -37,8 +37,9 @@ class CustomerController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Customer $customer)
     {
+      return view('customers.show', compact('customer'));
     }
 
     /**
