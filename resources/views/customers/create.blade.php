0b93f28d038add0926abfc2fe7e4de4d2bb93b01
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-30 17:25:32 +0200

    Website-Laravel

--- resources/views/customers/create.blade.php
+++ resources/views/customers/create.blade.php
@@ -5,8 +5,8 @@
 @section('content')
 <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center 
 bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-  <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Create New Customer</h2>
    <div>
+    <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Create New Customer</h2>
     <form action="" class="form bg-white p-6 border-1">
       <div>
         <label for="name" class="text-sm">Name :</label>
