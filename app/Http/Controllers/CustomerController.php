b209c453339cf33e373e7d2767ea907dc31c4bd8
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-30 17:40:13 +0200

    Website-Laravel

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -17,7 +17,7 @@ class CustomerController extends Controller
     public function index()
     {
       return view('customers.index', [
-        'customers' => self::getdata()
+        'customers' => Customer::all()
       ]);
     }
 
