bb3559dbbdbc3d925c74b1c084648bc5beecd906
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-31 00:47:40 +0200

    Website-laravel

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Customer;
-use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
