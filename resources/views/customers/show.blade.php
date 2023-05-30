8739024d1ed4535c8903980f9faa10f045208dd3
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-31 00:39:06 +0200

    Website-laravel

--- resources/views/customers/show.blade.php
+++ resources/views/customers/show.blade.php
@@ -1,5 +1,13 @@
+@extends('layouts.layout')
+@section('title')
+  Customers
+@endsection
+@section('content')
+<div class="bg-gray-100 min-h-screen">
 <ul>
   <li>{{$customer->id}}</li>
   <li>{{$customer->name}}</li>
   <li>{{$customer->details}}</li>
 </ul>
+</div>
+@endsection
