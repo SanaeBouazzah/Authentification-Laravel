0b213c59c5c1ab77d54058f33fc09bee9d9ff36c
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-30 17:11:05 +0200

    Website-Laravel

--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,5 @@ Route::get('/contact', [StaticController::class, 'contact'])->name('contact');
 
 Route::get('/customers', [CustomerController::class, 'index'])->name('customers.index');
 Route::get('/customers/show', [CustomerController::class, 'show'])->name('customers.show');
+Route::get('/customers/create', [CustomerController::class, 'create'])->name('customers.create');
 
