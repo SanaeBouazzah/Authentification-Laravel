a1c5ea9818df8582fff5cf275056059fa926b463
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-30 22:02:46 +0200

    Website-Laravel

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     use HasFactory;
+    
+    protected $table = ['customers'];
+
+    protected $fillable = [
+      'name',
+      'details'
+    ];
 }
