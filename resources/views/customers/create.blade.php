41e253105a77c3196b65971a72917fec9dc358f3
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-30 17:14:56 +0200

    Website-Laravel

--- resources/views/customers/create.blade.php
+++ resources/views/customers/create.blade.php
@@ -7,6 +7,20 @@
 bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
   <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Create New Customer</h2>
    <div>
+    <form action="">
+      <div>
+        <label for="name">Name :</label>
+        <input type="text" name="name" id="name">
+      </div>
+      <div>
+        <label for="email">Email :</label>
+        <input type="text" name="email" id="email">
+      </div>
+      <div>
+        <label for="password">Password :</label>
+        <input type="text" name="password" id="password">
+      </div>
+    </form>
    </div>
 </div>
 @endsection
