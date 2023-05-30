2e121dcba8118c9f96c9163d6db6b5a0129f52cd
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-30 18:27:07 +0200

    Website-Laravel

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
@@ -11,7 +12,7 @@ class CustomerController extends Controller
      */
     public function index()
     {
-        //
+        $customer = Customer::latest();
     }
 
     /**
