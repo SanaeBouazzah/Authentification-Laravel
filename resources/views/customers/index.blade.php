40022255a4d87028f133f1b93803243a19d2858d
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-31 00:23:20 +0200

    Website-laravel

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -21,6 +21,7 @@
         <td style="border:1px solid #000; padding:20px;">{{$customer->name}}</td>
         <td style="border:1px solid #000; padding:20px;">{{$customer->details}}</td>
         <td style="border:1px solid #000; padding:20px;"><a href="">SHOW</a></td>
+        <td style="border:1px solid #000; padding:20px;"><a href="">Delete</a></td>
     @endforeach
   </tr>
   </tbody>
