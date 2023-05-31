9e5cb77040f2a7849f562a8d344f858edf9df9aa
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-31 16:38:46 +0200

    Website-Laravel

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -19,8 +19,8 @@
     @foreach ($customers as $value)
         <td style="border:1px solid #000; padding:20px;">{{$value->id}}</td>
         <td style="border:1px solid #000; padding:20px;">{{$value->name}}</td>
-        <td style="border:1px solid #000; padding:20px;">{{$value->details}}</td>
-        <td style="border:1px solid #000; padding:20px;"><a href="{{route('customers.show', $customer->id)}}">SHOW</a></td>
+        <td style="border:1px solid #000; padding:20px;">{{$value['details']}}</td>
+        <td style="border:1px solid #000; padding:20px;"><a href="{{route('customers.show', $value->id)}}">SHOW</a></td>
         {{-- <td style="border:1px solid #000; padding:20px;"><a href="{{route('customers.delete')}}">Delete</a></td> --}}
     @endforeach
   </tr>
