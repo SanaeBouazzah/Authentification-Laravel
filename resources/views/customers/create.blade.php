98a219ce22469f2401f15792a9248a52bb3d2127
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-31 16:58:26 +0200

    Website-Laravel

--- resources/views/customers/create.blade.php
+++ resources/views/customers/create.blade.php
@@ -4,5 +4,19 @@
 @endsection
 @section('content')
 
+<form action="">
+  @csrf
+  <div class="mb-3">
+    <label>Name:</label>
+     <input type="text" name="name" class="form-control" value="{{old('name')}}">
+   </div>
+   <div class="mb-3">
+    <label>details:</label>
+     <input type="text" name="email" class="form-control" value="{{old('email')}}">
+   </div>
+   <div class="d-grid">
+     <button class="btn btn-primary btn-block my-2">Ajouter</button>
+   </div>
+ </form>
 
 @endsection
