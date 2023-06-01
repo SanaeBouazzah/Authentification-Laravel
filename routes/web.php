69a1bec4f74cbf0ac4364ca479aad84da24938bd
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-02 01:56:41 +0200

    Website-Laravel

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,5 @@ Route::post('/customers/store', [CustomerController::class, 'store'])->name('cus
 
 Route::delete('customers/{customer}', [CustomerController::class, 'destroy'])->name('customers.destroy');
 Route::get('customers/{id}', [CustomerController::class, 'show'])->name('customers.show');
+Route::get('customers/{id}', [CustomerController::class, 'update'])->name('customers.update');
 
