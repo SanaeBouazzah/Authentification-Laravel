2528869b77fa6a975caa47e6a97b7ecbcaa81243
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-03 02:14:09 +0200

    Website-Laravel

--- resources/views/customers/show.blade.php
+++ resources/views/customers/show.blade.php
@@ -3,11 +3,14 @@
   Customers
 @endsection
 @section('content')
-<div class="bg-gray-100 min-h-screen">
-<ul>
-  <li>{{$customer->id}}</li>
-  <li>{{$customer->name}}</li>
-  <li>{{$customer->details}}</li>
-</ul>
+<div class="container">
+  <div class="bg-gray-100 min-h-screen">
+    <a href="{{route('customers.index')}}" class="btn btn-primary m-4 px-4">Back</a>
+  <ul>
+    <li>{{$customer->id}}</li>
+    <li>{{$customer->name}}</li>
+    <li>{{$customer->details}}</li>
+  </ul>
+  </div>
 </div>
 @endsection
