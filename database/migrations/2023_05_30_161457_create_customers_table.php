760c8a2d928426c036890ebf207962641cf28119
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-11 16:42:23 +0200

    Website-Laravel

--- database/migrations/2023_05_30_161457_create_customers_table.php
+++ database/migrations/2023_05_30_161457_create_customers_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            $table->string('image');
             $table->string('name');
             $table->text('details');
             $table->timestamps();
