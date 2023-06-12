daaa8d11b0142933c26a648baa465c3b8c9af623
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-12 15:02:24 +0200

    Website-Laravel

--- database/migrations/2023_06_12_130152_create_photos_table.php
+++ database/migrations/2023_06_12_130152_create_photos_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('photos', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('size');
             $table->timestamps();
         });
     }
