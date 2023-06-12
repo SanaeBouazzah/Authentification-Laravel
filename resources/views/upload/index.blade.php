f087dbcd099710d5fadd36048d6f7baf0188f1d1
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-12 15:00:58 +0200

    Website-Laravel

--- resources/views/upload/index.blade.php
+++ resources/views/upload/index.blade.php
@@ -1,11 +1,8 @@
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
-<body>
-  
-</body>
-</html>
+@extends('layouts.layout')
+@section('title')
+  Customers
+@endsection
+@section('content')
+
+upload
+@endsection
