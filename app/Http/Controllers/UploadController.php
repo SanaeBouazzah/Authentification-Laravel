f4f0dce881b1782fb15e921c127f31cb45b52267
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-13 14:47:54 +0200

    Website-Laravel

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -15,16 +15,21 @@ class UploadController extends Controller
     public function store(Request $request)
     {
       if($request->has('photo')){
-        $file = $request->photo;
+        $file = $request->photo; //recuperer le nom de champ
+        $photo_name = time() . '_' . $file->getClientOriginalName(); //recuperer le nom de fichier
+        $file->move(public_path('uploads'), $photo_name);
       }
-      $size = $request->file('photo')->getSize();
-      $name = $request->file('photo')->getClientOriginalName();
-      $request->file('photo')->store('public/images');
+      // $size = $request->file('photo')->getSize();
+      // $name = $request->file('photo')->getClientOriginalName();
+      // $request->file('photo')->store('public/images');
     
-      $photo = new Upload();
-      $photo->name = $name;
-      $photo->size = $size;
-      $photo->save();
+      // $photo = new Upload();
+      // $photo->name = $name;
+      // $photo->size = $size;
+      // $photo->save();
+      Upload::create([
+        'photo' => $photo_name,
+      ]);
     
       return redirect()->back()->with('success', 'you have added image successfully.');
     }
