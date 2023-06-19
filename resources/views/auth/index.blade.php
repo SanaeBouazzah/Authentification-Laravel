6cbe57a1639cc83c9cf60e1c698b32c2c4eb2c09
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-19 17:38:45 +0200

    Website-Laravel

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -4,4 +4,17 @@
 @endsection
 @section('content')
 
+
+
+   <form action="" method="post">
+    @csrf
+    <div>
+      <label for="user">Username or Email :</label>
+      <input type="text" name="user" id="user">
+    </div>
+    <div>
+      <label for="password">Password :</label>
+      <input type="password" name="password" id="password">
+    </div>
+   </form>
 @endsection
