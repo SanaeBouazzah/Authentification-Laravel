9b71f68b9df22bc061178a4fb8a829e2f5bee817
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-19 19:19:47 +0200

    Website-Laravel

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('user')->unique();
+            $table->string('name');
+            $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
