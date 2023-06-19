2ec7ea79e43f0c7641b2f055401fe28951cd3ed2
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-19 18:49:06 +0200

    Website-Laravel

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -19,32 +19,24 @@
                     <div class="d-flex flex-row align-items-center mb-4">
                       <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                       <div class="form-outline flex-fill mb-0">
-                        <input type="text" id="form3Example1c" class="form-control" />
-                        <label class="form-label" for="form3Example1c">Your Name</label>
+                        <input type="text" id="name" class="form-control" name="name"/>
+                        <label class="form-label" for="name">Your Name</label>
                       </div>
                     </div>
   
                     <div class="d-flex flex-row align-items-center mb-4">
                       <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                       <div class="form-outline flex-fill mb-0">
-                        <input type="email" id="form3Example3c" class="form-control" />
-                        <label class="form-label" for="form3Example3c">Your Email</label>
+                        <input type="email" id="email" class="form-control" name="email"/>
+                        <label class="form-label" for="email">Your Email</label>
                       </div>
                     </div>
   
                     <div class="d-flex flex-row align-items-center mb-4">
                       <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
                       <div class="form-outline flex-fill mb-0">
-                        <input type="password" id="form3Example4c" class="form-control" />
-                        <label class="form-label" for="form3Example4c">Password</label>
-                      </div>
-                    </div>
-  
-                    <div class="d-flex flex-row align-items-center mb-4">
-                      <i class="fas fa-key fa-lg me-3 fa-fw"></i>
-                      <div class="form-outline flex-fill mb-0">
-                        <input type="password" id="form3Example4cd" class="form-control" />
-                        <label class="form-label" for="form3Example4cd">Repeat your password</label>
+                        <input type="password" id="password" class="form-control"  name="password"/>
+                        <label class="form-label" for="password">Password</label>
                       </div>
                     </div>
   
