505d241d6e514734d725bae43e42011312f6c778
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-19 17:29:28 +0200

    Website-Laravel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,10 +15,6 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         \App\Models\User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-        \App\Models\User::factory()->create([
             'name' => 'ali',
             'email' => 'ali@example.com',
         ]);
