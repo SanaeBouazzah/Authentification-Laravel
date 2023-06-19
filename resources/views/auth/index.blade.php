1a01d3d1132b5f9a910b3c0e1ce59f11e99a628b
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-19 17:41:16 +0200

    Website-Laravel

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -6,18 +6,18 @@
 
 
 
-   <form action="" method="post">
+   <form action="" method="post" class="">
     @csrf
     <div>
       <label for="user">Username or Email :</label>
-      <input type="text" name="user" id="user">
+      <input type="text" name="user" id="user" class="form-control">
     </div>
     <div>
       <label for="password">Password :</label>
-      <input type="password" name="password" id="password">
+      <input type="password" name="password" id="password" class="form-control">
     </div>
     <div>
-      <input type="submit" value="Log In">
+      <input type="submit" value="Log In" class="form-control">
     </div>
    </form>
 @endsection
