c2ff0bc5abf31203a080929bf30d6cfaa8a15ad0
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-19 18:29:01 +0200

    Website-Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\CustomerController;
-use App\Http\Controllers\LoginController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\StaticController;
 use App\Http\Controllers\UploadController;
 use Illuminate\Support\Facades\Route;
